79603a5527a59a90c1d0fcd870ee96fc17eb056c
Andriy <andrden@gmail.com>
2018-03-08 11:45:22 +0200

    Even for 1 register: roadFood=87 eatFood=78 eatNoFood=43 of 1000 steps is too low, well over 100 food must be eaten, because 'fwd' must be executed until food is seen, not random 'take'-s which only take time and produce no results in the long run

--- src/main/java/bumblebee/v2/Main.java
+++ src/main/java/bumblebee/v2/Main.java
@@ -6,10 +6,10 @@ public class Main {
 //            new bumblebee.v2.tests.min_adapt_incentive.World();
 //            new bumblebee.v2.tests.first_below_zero.World();
 //            new bumblebee.v2.tests.min_sensor_initiative.World();
-//            new bumblebee.v2.tests.min_writable_register.World();
+            new bumblebee.v2.tests.min_writable_register.World();
 //            new bumblebee.v2.tests.false_sensors.World();
            // new bumblebee.v2.tests.false_sensors_register.World();
-            new bumblebee.v2.tests.two_registers.World();
+           // new bumblebee.v2.tests.two_registers.World();
             //            bumblebee.v2.tests.world2d.View.main(new String[0]);
         } catch (Exception e) {
             System.out.println("PROBLEM:::: !!!!!!! EXCEPTION ===============");
