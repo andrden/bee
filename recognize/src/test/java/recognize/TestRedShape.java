7806e1db449944d6c881dda6a4afd959825ac889
Andriy <andrden@gmail.com>
2018-05-17 10:18:33 +0300

    puzzle with blurred line - there must be a border point visually

--- recognize/src/test/java/recognize/TestRedShape.java
+++ recognize/src/test/java/recognize/TestRedShape.java
@@ -16,6 +16,8 @@ import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestRedShape {
 
@@ -52,6 +54,44 @@ public class TestRedShape {
         }
 
         assertEquals(expectedCountCurves, c.finalCurves.size());
+    }
+
+    @Test
+    void testBlur() throws Exception {
+        File dir = new File("/tmp/imgDebug/testBlue");
+        dir.mkdirs();
+
+        BufferedImage img = ImageIO.read(getClass().getClassLoader().getResourceAsStream("hearts-blurry2.png"));
+
+        BufferedImage colors = new BufferedImage(256 * 3 + (int) (256 * Math.sqrt(3)), img.getHeight(), img.getType());
+        Graphics gcolors = colors.getGraphics();
+        gcolors.fillRect(0, 0, colors.getWidth(), colors.getHeight());
+        for (int y = 0; y < img.getHeight(); y++) {
+            int rgb = img.getRGB(185, y);
+            var color = new Color(rgb);
+            int fromRed = (int) Colors.distance(Color.red.getRGB(), rgb);
+            colors.setRGB(color.getRed(), y, Color.red.getRGB());
+            colors.setRGB(256 + color.getGreen(), y, Color.green.getRGB());
+            colors.setRGB(256 * 2 + color.getBlue(), y, Color.blue.getRGB());
+            colors.setRGB(256 * 3 + fromRed, y, Color.blue.getRGB());
+
+            colors.setRGB(256, y, Color.black.getRGB());
+            colors.setRGB(256 * 2, y, Color.black.getRGB());
+            colors.setRGB(256 * 3, y, Color.black.getRGB());
+        }
+        ImageIO.write(colors, "png", new File(dir, "hearts-blurry2-colors.png"));
+
+        var c = new CurvesExtractor("", img);
+        for (int y = 0; y < img.getHeight(); y++) {
+            img.setRGB(185, y, Color.gray.getRGB());
+        }
+        c.aroundRed.keySet().forEach(p -> {
+            img.setRGB(p.x, p.y, Color.blue.getRGB());
+        });
+        ImageIO.write(img, "png", new File(dir, "hearts-blurry2.png"));
+
+        long count = c.aroundRed.keySet().stream().filter(p -> p.x == 185 && (p.y >= 250 && p.y <= 265)).count();
+        assertNotEquals(0L, count);
     }
 
 }
