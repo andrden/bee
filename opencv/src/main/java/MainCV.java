c56c37c9da74c73753f1cbd5805d14f1cf5fc909
Andriy <andrden@gmail.com>
2018-07-05 22:43:43 +0300

    one side of second card found

--- opencv/src/main/java/MainCV.java
+++ opencv/src/main/java/MainCV.java
@@ -76,8 +76,15 @@ public class MainCV {
             lines.add(new Line(new Point(l[0], l[1]), new Point(l[2], l[3])));
         }
         lines.sort(Comparator.comparingDouble(Line::len2).reversed());
-        for (int i = 0; i < 1; i++) {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
             Line line1 = lines.get(i);
+
+            if (cards.stream().anyMatch(card -> card.intersects(line1))) {
+                System.out.println(i + " line skipped");
+                continue;
+            }
+            System.out.println(i + " line accepted");
 
             Line line2 = closestDirection(lines, line1);
 
@@ -89,7 +96,7 @@ public class MainCV {
             System.out.println(line1.directionDiff(line2));
 
             List<Line> linesAcross = lines.stream().filter(l ->
-                    line1.side(l.p1) * line2.side(l.p1) < 0 && line1.side(l.p2) * line2.side(l.p2) < 0
+                    isBetween(l, line1, line2)
                             && l.len2() > Math.pow(300, 2)
                             && Util.sameSign(l.side(line1.p1), l.side(line1.p2), l.side(line2.p1), l.side(line2.p2)))
                     .collect(Collectors.toList());
@@ -97,15 +104,55 @@ public class MainCV {
 //            for (Line l : linesAcross) {
 //                Imgproc.line(dest, l.p1, l.p2, new Scalar(0, 255, 255), 7, Imgproc.LINE_AA, 0);
 //            }
-            Line across1 = linesAcross.get(0); // longest
-            Imgproc.line(dest, across1.p1, across1.p2, new Scalar(0, 255, 255), 7, Imgproc.LINE_AA, 0);
+            if (linesAcross.size() > 0) {
+                Line across1 = linesAcross.get(0); // longest
+                Imgproc.line(dest, across1.p1, across1.p2, new Scalar(0, 255, 255), 7, Imgproc.LINE_AA, 0);
 
-            Line across2 = closestDirection(linesAcross, across1);
-            Imgproc.line(dest, across2.p1, across2.p2, new Scalar(0, 255, 255), 7, Imgproc.LINE_AA, 0);
-            System.out.println(across1.vector() + " len=" + Math.sqrt(across1.len2()));
-            System.out.println(across2.vector() + " len=" + Math.sqrt(across2.len2()));
-            System.out.println(across1.directionDiff(across2));
+                Line across2 = closestDirection(linesAcross, across1);
+                Imgproc.line(dest, across2.p1, across2.p2, new Scalar(0, 255, 255), 7, Imgproc.LINE_AA, 0);
+                System.out.println(across1.vector() + " len=" + Math.sqrt(across1.len2()));
+                System.out.println(across2.vector() + " len=" + Math.sqrt(across2.len2()));
+                System.out.println(across1.directionDiff(across2));
 
+                Card card = new Card(line1, line2, across1, across2);
+                cards.add(card);
+            }
+        }
+        cards.get(0).warp(dest, img, imgFile);
+
+        ImageIO.write(Utils.matToBufferedImage(dest), "png", new File(imgFile + ".edges.png"));
+    }
+
+    private boolean isBetween(Line l, Line line1, Line line2) {
+        return line1.side(l.p1) * line2.side(l.p1) < 0 && line1.side(l.p2) * line2.side(l.p2) < 0;
+    }
+
+    private static boolean isBetween(Point p, Line line1, Line line2) {
+        return line1.side(p) * line2.side(p) < 0;
+    }
+
+    static class Card {
+        Line line1, line2;
+        Line across1, across2;
+
+        public Card(Line line1, Line line2, Line across1, Line across2) {
+            this.line1 = line1;
+            this.line2 = line2;
+            this.across1 = across1;
+            this.across2 = across2;
+        }
+
+        boolean intersects(Line line) {
+            return (isBetween(line.p1, line1, line2) && isBetween(line.p1, across1, across2)) ||
+                    (isBetween(line.p2, line1, line2) && isBetween(line.p2, across1, across2)) ||
+                    line1.touchingDistance(line) < 50 ||
+                    line2.touchingDistance(line) < 50 ||
+                    across1.touchingDistance(line) < 50 ||
+                    across2.touchingDistance(line) < 50
+                    ;
+        }
+
+        void warp(Mat dest, Mat img, String imgFile) throws IOException {
             Point int00 = intersection(line2, across1);
             Imgproc.circle(dest, int00, 10, new Scalar(100, 255, 255));
 
@@ -115,27 +162,25 @@ public class MainCV {
             Point int01 = intersection(line2, across2);
             Imgproc.circle(dest, int01, 10, new Scalar(100, 255, 255));
 
-            int w = 3*225;
-            int h = 3*350;
+            int w = 3 * 225;
+            int h = 3 * 350;
             Mat transform = Imgproc.getAffineTransform(new MatOfPoint2f(int00, int10, int01),
-                    new MatOfPoint2f(new Point(0,0), new Point(w,0), new Point(0,h)));
+                    new MatOfPoint2f(new Point(0, 0), new Point(w, 0), new Point(0, h)));
 
             Mat warp = new Mat(); // will hold the results of the detection
-            Imgproc.warpAffine(img, warp, transform, new Size(w,h), INTER_CUBIC, BORDER_CONSTANT, new Scalar(0,0,0));
+            Imgproc.warpAffine(img, warp, transform, new Size(w, h), INTER_CUBIC, BORDER_CONSTANT, new Scalar(0, 0, 0));
 
             ImageIO.write(Utils.matToBufferedImage(warp), "png", new File(imgFile + ".warp.png"));
         }
-
-        ImageIO.write(Utils.matToBufferedImage(dest), "png", new File(imgFile + ".edges.png"));
     }
 
-    Point intersection(Line l1, Line l2) {
+    static Point intersection(Line l1, Line l2) {
         return intersection(l1.p1, l1.p2, l2.p1, l2.p2);
     }
 
     // Finds the intersection of two lines, or returns false.
 // The lines are defined by (o1, p1) and (o2, p2).
-    Point intersection(Point o1, Point p1, Point o2, Point p2) {
+    static Point intersection(Point o1, Point p1, Point o2, Point p2) {
         Point x = Util.sub(o2, o1);
         Point d1 = Util.sub(p1, o1);
         Point d2 = Util.sub(p2, o2);
